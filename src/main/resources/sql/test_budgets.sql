afb847c35493afa41149cf16239e0ffc4989eeb8
DESKTOP-MUAA09M\JayDamon <jay.n.damon@gmail.com>
2019-11-06 07:45:43 -0500

    Add blank budgets to load

--- src/main/resources/sql/test_budgets.sql
+++ src/main/resources/sql/test_budgets.sql
@@ -1,32 +1,65 @@
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (1, 2, 'Rent / Mortgage', '2018-01-01', NULL, 2, 800, 1);
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (2, 2, 'Auto Loan / Lease', '2018-01-01', NULL, 2, 350, 1);
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (29, 2, 'Auto Insurance', '2018-01-01', NULL, 2, 350, 1);
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (28, 2, 'Health Insurance', '2018-01-01', NULL, 2, 350, 1);
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (3, 2, 'Cable / Internet', '2018-01-01', NULL, 2, 102.25, 1);
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (4, 2, 'Cell Phone', '2018-01-01', NULL, 2, 75.32, 1);
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (5, 6, 'Groceries', '2018-01-01', NULL, 2, 100, 1);
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (6, 6, 'Gas', '2018-01-01', NULL, 2, 100, 1);
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (7, 6, 'Household Items', '2018-01-01', NULL, 2, 50, 1);
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (8, 7, 'Amazon Prime', '2018-01-01', NULL, 2, 99, 1);
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (9, 7, 'Plex', '2018-01-01', NULL, 2, 3.99, 1);
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (10, 7, 'Netflix', '2018-01-01', NULL, 2, 10.99, 1);
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (27, 7, 'spotify', '2018-01-01', NULL, 2, 10.99, 1);
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (30, 7, 'Adobe', '2018-01-01', '2018-12-31', 2, 20.39, 1);
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (26, 7, 'Audible', '2018-01-01', NULL, 2, 20.39, 1);
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (11, 8, 'Rainy Day Fund', '2018-01-01', NULL, 2, 100, 1);
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (12, 8, 'Home Downpayment', '2018-01-01', NULL, 2, 1000, 1);
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (13, 8, 'Vacation', '2018-01-01', NULL, 2, 200, 1);
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (14, 9, 'Retirement / 401k', '2018-01-01', NULL, 2, 50, 1);
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (15, 1, 'General', '2018-01-01', NULL, 2, 50, 1); -- fixed income
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (16, 2, 'General', '2018-01-01', NULL, 2, 50, 1); -- fixed bill
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (17, 3, 'General', '2018-01-01', NULL, 2, 50, 1); -- fixed spending
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (18, 4, 'General', '2018-01-01', NULL, 2, 50, 1); -- flexible income
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (19, 5, 'General', '2018-01-01', NULL, 2, 50, 1); -- flexible bill
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (20, 6, 'General', '2018-01-01', NULL, 2, 50, 1); -- flexible spending
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (21, 7, 'General', '2018-01-01', NULL, 2, 50, 1); -- discretionary subscription
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (22, 8, 'General', '2018-01-01', NULL, 2, 50, 1); -- discretionary savings
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (23, 9, 'General', '2018-01-01', NULL, 2, 50, 1); -- discretionary investment
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (24, 10, 'General', '2018-01-01', NULL, 2, 50, 1); -- discretionary spending
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (25, 10, 'Dining', '2018-01-01', NULL, 2, 50, 1);
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (31, 10, 'Dining', '2017-01-01', '2017-12-31', 2, 50, 0);
-INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (32, 10, 'Dining', '2017-01-01', '2017-12-31', 2, 50, 0);
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (1, 2, 'Rent / Mortgage', '2018-01-01', NULL, 2, 800, 1);
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (2, 2, 'Auto Loan / Lease', '2018-01-01', NULL, 2, 350, 1);
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (29, 2, 'Auto Insurance', '2018-01-01', NULL, 2, 350, 1);
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (28, 2, 'Health Insurance', '2018-01-01', NULL, 2, 350, 1);
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (3, 2, 'Cable / Internet', '2018-01-01', NULL, 2, 102.25, 1);
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (4, 2, 'Cell Phone', '2018-01-01', NULL, 2, 75.32, 1);
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (5, 6, 'Groceries', '2018-01-01', NULL, 2, 100, 1);
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (6, 6, 'Gas', '2018-01-01', NULL, 2, 100, 1);
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (7, 6, 'Household Items', '2018-01-01', NULL, 2, 50, 1);
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (8, 7, 'Amazon Prime', '2018-01-01', NULL, 2, 99, 1);
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (9, 7, 'Plex', '2018-01-01', NULL, 2, 3.99, 1);
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (10, 7, 'Netflix', '2018-01-01', NULL, 2, 10.99, 1);
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (27, 7, 'spotify', '2018-01-01', NULL, 2, 10.99, 1);
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (30, 7, 'Adobe', '2018-01-01', '2018-12-31', 2, 20.39, 1);
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (26, 7, 'Audible', '2018-01-01', NULL, 2, 20.39, 1);
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (11, 8, 'Rainy Day Fund', '2018-01-01', NULL, 2, 100, 1);
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (12, 8, 'Home Downpayment', '2018-01-01', NULL, 2, 1000, 1);
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (13, 8, 'Vacation', '2018-01-01', NULL, 2, 200, 1);
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (14, 9, 'Retirement / 401k', '2018-01-01', NULL, 2, 50, 1);
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (15, 1, 'General', '2018-01-01', NULL, 2, 50, 1); -- fixed income
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (16, 2, 'General', '2018-01-01', NULL, 2, 50, 1); -- fixed bill
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (17, 3, 'General', '2018-01-01', NULL, 2, 50, 1); -- fixed spending
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (18, 4, 'General', '2018-01-01', NULL, 2, 50, 1); -- flexible income
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (19, 5, 'General', '2018-01-01', NULL, 2, 50, 1); -- flexible bill
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (20, 6, 'General', '2018-01-01', NULL, 2, 50, 1); -- flexible spending
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (21, 7, 'General', '2018-01-01', NULL, 2, 50, 1); -- discretionary subscription
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (22, 8, 'General', '2018-01-01', NULL, 2, 50, 1); -- discretionary savings
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (23, 9, 'General', '2018-01-01', NULL, 2, 50, 1); -- discretionary investment
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (24, 10, 'General', '2018-01-01', NULL, 2, 50, 1); -- discretionary spending
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (25, 10, 'Dining', '2018-01-01', NULL, 2, 50, 1);
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (31, 10, 'Dining', '2017-01-01', '2017-12-31', 2, 50, 0);
+-- INSERT INTO budget (budget_id, budget_category_id, budget_item_name, start_date, end_date, frequency_type_id, amount, in_use) VALUES (32, 10, 'Dining', '2017-01-01', '2017-12-31', 2, 50, 0);
+
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (1, 2, 'Rent / Mortgage');
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (2, 2, 'Auto Loan / Lease');
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (29, 2, 'Auto Insurance');
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (28, 2, 'Health Insurance');
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (3, 2, 'Cable / Internet');
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (4, 2, 'Cell Phone');
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (5, 6, 'Groceries');
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (6, 6, 'Gas');
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (7, 6, 'Household Items');
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (8, 7, 'Amazon Prime');
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (9, 7, 'Plex');
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (10, 7, 'Netflix');
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (27, 7, 'spotify');
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (30, 7, 'Adobe');
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (26, 7, 'Audible');
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (11, 8, 'Rainy Day Fund');
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (12, 8, 'Home Downpayment');
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (13, 8, 'Vacation');
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (14, 9, 'Retirement / 401k');
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (15, 1, 'General'); -- fixed income
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (16, 2, 'General'); -- fixed bill
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (17, 3, 'General'); -- fixed spending
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (18, 4, 'General'); -- flexible income
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (19, 5, 'General'); -- flexible bill
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (20, 6, 'General'); -- flexible spending
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (21, 7, 'General'); -- discretionary subscription
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (22, 8, 'General'); -- discretionary savings
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (23, 9, 'General'); -- discretionary investment
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (24, 10, 'General'); -- discretionary spending
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (25, 10, 'Dining');
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (31, 10, 'Dining');
+INSERT INTO budget (budget_id, budget_category_id, budget_item_name) VALUES (32, 10, 'Dining');
